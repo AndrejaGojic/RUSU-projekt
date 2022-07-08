8ce046e365ad22d3219f08e9dce36471b887741f
AndrejaGojic <andrejagojic1810@gmail.com>
2022-07-08 12:04:52 +0200

    modified

--- Kod/alex.py
+++ Kod/alex.py
@@ -13,33 +13,49 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, BatchN
 
 img_size = 96
 
-def extract_label(img_path,train = True):
+def extract_id(img_path,real):
   filename, _ = os.path.splitext(os.path.basename(img_path))
 
   subject_id, etc = filename.split('__')
   
-  if train:
-      gender, lr, finger, _, _ = etc.split('_')
-  else:
+  if real:
       gender, lr, finger, _ = etc.split('_')
+  else:
+      gender, lr, finger, _, _ = etc.split('_')
+  
+  gender = 0 if gender == 'M' else 1
+
+  return np.array([subject_id], dtype=np.uint16)
+
+def extract_gender(img_path,real):
+  filename, _ = os.path.splitext(os.path.basename(img_path))
+
+  subject_id, etc = filename.split('__')
+  
+  if real:
+      gender, lr, finger, _ = etc.split('_')
+  else:
+      gender, lr, finger, _, _ = etc.split('_')
   
   gender = 0 if gender == 'M' else 1
 
   return np.array([gender], dtype=np.uint16)
 
 
-def loading_data(path,train):
+def loading_data(path,real):
     print("loading data from: ",path)
     data = []
     for img in os.listdir(path):
         try:
-            img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+            img_array = cv2.imread(os.path.join(path, img))
             img_resize = cv2.resize(img_array, (img_size, img_size))
-            label = extract_label(os.path.join(path, img),train)
-            data.append([label[0], img_resize ])
+            subject_id = extract_id(os.path.join(path, img), real)
+            gender = extract_gender(os.path.join(path, img), real)
+            data.append([subject_id[0], gender[0], img_resize])
         except Exception as e:
             pass
     data
+    print("loaded: ", len(data))
     return data
 
 
@@ -52,31 +68,60 @@ Hard_path = "SOCOFing/Altered/Altered-Hard"
 Easy_data = loading_data(Easy_path, train = True)
 Medium_data = loading_data(Medium_path, train = True)
 Hard_data = loading_data(Hard_path, train = True)
-test = loading_data(Real_path, train = False)
+Real_data = loading_data(Real_path, train = False)
 
-data = np.concatenate([Easy_data, Medium_data, Hard_data], axis=0)
+data = np.concatenate([Easy_data, Hard_data, Real_data], axis=0)
 
-del Easy_data, Medium_data, Hard_data
+data = data[data[:,0].argsort()]
+gender = data[:, 1]
+numb_male = len(gender[gender == 0])
+numb_female = len(gender[gender == 1])
+print('Broj muskaraca: ', numb_male)
+print('Broj zena: ', numb_female)
+
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+lable= ['Muskarci', ' Zene']
+numb = [numb_male, numb_female]
+ax.bar(lable[0], numb[0])
+ax.bar(lable[1], numb[1], color = 'red')
+plt.show()
+
+del Easy_data, Hard_data, Real_data, Real_path, Easy_path, Hard_path
+
+split_test =int(0.2513 * len(data))
+split_train = len(data) - split_test
+
+train_data = data[:split_train, :]
+test_data = data[split_train:, :]
+
+random.shuffle(train_data)
+random.shuffle(test_data)
 
 
-
-random.shuffle(data)
-random.shuffle(test)
-
-
-data
-
-
-
-
-img, labels = [], []
-for label, feature in data:
-    labels.append(label)
+img, train_labels = [], []
+for subject_id, label, feature in train_data:
+    train_labels.append(label)
     img.append(feature)
 train_data = np.array(img).reshape(-1, img_size, img_size, 1)
 train_data = train_data / 255.0
 
-train_labels = to_categorical(labels, num_classes = 2)
+train_labels = to_categorical(train_labels, num_classes = 2)
+
+
+
+del img
+img, test_labels = [], []
+for subject_id, label, feature in test_data:
+    test_labels.append(label)
+    img.append(feature)
+test_data = np.array(img).reshape(-1, img_size, img_size, 1)
+test_data = test_data / 255.0
+
+test_labels = to_categorical(test_labels, num_classes = 2)
+
+
+del data, img, subject_id, label, feature
 
 alex = Sequential()
 
@@ -103,20 +148,19 @@ alex.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
 
 alex.add(Flatten())
 
-alex.add(Dense(9216, activation = 'relu'))
-alex.add(Dense(4096, activation = 'relu'))
-alex.add(Dense(4096, activation = 'relu'))
+alex.add(Dense(1024, activation = 'relu'))
+alex.add(Dense(128, activation = 'relu'))
 alex.add(Dense(2, activation = 'sigmoid'))
 
 alex.summary()
 
 
 alex.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
-
-early_stopping_cb = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+early_stopping_cb = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
 
 history = alex.fit(train_data, train_labels, batch_size = 128, epochs = 15, 
-          validation_split = 0.2, callbacks = [early_stopping_cb], verbose = 1)
+          validation_split = 0.25, callbacks = [early_stopping_cb], verbose = 1)
+
 
 
 pd.DataFrame(history.history).plot(figsize = (8,5))
@@ -124,26 +168,10 @@ plt.grid(True)
 plt.gca().set_ylim(0,1)
 
 
-test_images, test_labels = [], []
-
-for label, feature in test:
-    test_images.append(feature)
-    test_labels.append(label)
-    
-test_images_resize = np.array(test_images).reshape(-1, img_size, img_size, 1)
-test_images_resize = test_images_resize / 255.0
-del test
-test_labels  = to_categorical(test_labels, num_classes = 2)
-test_images
-
-
-#alex.save('alexnet_model/')
-
-
-res = alex.evaluate(test_images_resize, test_labels)
+res = alex.evaluate(test_data, test_labels)
 print("    Test Loss: {:.5f}".format(res[0]))
 print("Test Accuracy: {:.2f}%".format(res[1] * 100))
-pred = alex.predict(test_images_resize)
+pred = alex.predict(test_data)
 pred = np.argmax(pred,axis=1)
 
 
@@ -166,4 +194,4 @@ for i in range(1, (n_img[0] * n_img[1])+1):
         pred_title = 'M'
         
     plt.title(f"True: {test_title}\nPredicted: {pred_title}", color=color)
-    plt.imshow(test_images[i], cmap='gray')
+    plt.imshow(test_data[i], cmap='gray')
